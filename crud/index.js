d9d6885280f3aa0e8376755314eb6361d06ca4cf
Esterzinha12 <ester_girelli@estudante.sesisenai.org.br>
2022-06-20 21:47:55 -0300

    2

--- crud/index.js
+++ crud/index.js
@@ -1,5 +1,14 @@
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const { initializeApp } = require('firebase/app');
+const { getFirestore,
+    collection,
+    doc,
+    setDoc,
+    addDoc,
+    query,
+    were,
+    getDocs,
+    getDoc,
+    deleteDoc } = require('firebase/firestore/lite');
 
 const firebaseConfig = {
     apiKey: "AIzaSyAN09Clb3tpYdAY_u1FYOGtHX8B6Dlsvq0",
@@ -9,32 +18,73 @@ const firebaseConfig = {
     messagingSenderId: "487412008681",
     appId: "1:487412008681:web:daa9b88b9011f1ce691278",
     measurementId: "G-EBXLPCQ6Z2"
-  };
+};
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 const db = getFirestore();
 
-async function salvar(nomeTabela, id, dado){
-    if(id){
-        const referenceEnity = await db.collection(nomeTabela).doc(id).set(dado);
+async function salvar(nomeTabela, id, dado) {
+    if (id) {
+        const referenceEnity = await setDoc(doc(db, nomeTabela, id), dado);
         const savedData = {
             ...dado,
-            id:id
+            id: id
         }
         return savedData;
-    }else{
-       const referenceEnity= await db.collection(nomeTabela).add(dado);
-       const savedData = {
-        ...dado,
-        id:referenceEnity.id
-       }
-       return savedData;
+    } else {
+        const referenceEnity = await addDoc(collection(db, nomeTabela), dado);
+        const savedData = {
+            ...dado,
+            id: referenceEnity.id
+        }
+        return savedData;
     }
-    
+
 }
 
-modules.export={
-    save
+async function get(nomeTabela) {
+
+    const tableRef = collection(db, nomeTabela);
+
+    const q = query(tableRef);
+
+    const querySnapshot = await getDocs(q);
+
+    const lista = []
+    querySnapshot.forEach((doc) => {
+        const data = {
+            ...doc.data(),
+            id: doc.id
+        }
+        lista.push(data);
+
+    });
+    return lista;
+}
+
+async function getById(nomeTabela, id){
+    const docRef = doc(db, nomeTabela, id);
+    const docSnap = await getDoc(docRef);
+    
+    if (docSnap.exists()) {
+      return docSnap.data();
+    } else {
+      return new Error ("Not found!");
+    }
+
+}
+
+async function deletar(nomeTabela, id){
+    const dado= await deleteDoc(doc{db, nomeTabela, id})
+    return{
+        messege:`${id} deleted`
+    }
+}
+
+
+module.exports = {
+    salvar,
+    get
 }
 
--- sandBox.js
+++ sandBox.js
@@ -1,3 +1,18 @@
 const crud = require("./crud");
 
-crud.save("pessoas", undefined, {nome: "Ester", sobrenome: "Girelli"})
+async function buscarDados(){
+    const dados = await crud.get("pessoas");
+    console.log(dados);
+}
+
+async function buscarDadosId(){
+    const dados = await crud.getById("pessoas", '5S2bsktIRK3s9ZIFilpc')
+    console.log(dados);
+}
+
+async function (){
+    const dados = await crud.remove("pessoas", )
+    console.log(dados);
+}
+
+buscarDados();
